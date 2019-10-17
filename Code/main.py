e008514a5660fdf3adaec4ea083782bb460b7d05
Hammie217 <hammie217@gmail.com>
2019-10-17 12:25:34 +0100

    Added boundary masks

--- Code/main.py
+++ Code/main.py
@@ -11,9 +11,29 @@ while(True):
 
     # Our operations on the frame come here
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    #Blur
+    gray = cv2.GaussianBlur(gray,(5,5),0)
 
     # Find the edges in the image using canny detector
     edges = cv2.Canny(gray, 100, 200)
+
+    #create and multiply ROI
+    height = edges.shape[0]
+    width = edges.shape[1]
+    rectangle = np.array([ [(width/10,height), (width/5,height), (width/5,0), (width/10,0)] , [(9*width/10 ,height), (4*width/5 ,height), (4*width/5 ,0), (9*width/10 ,0)] ], np.int32)
+    mask = np.zeros_like(edges)
+    x1,y1 = rectangle[0,0]
+    x2,y2 = rectangle[0,2]
+    x3,y3 = rectangle[1,0]
+    x4,y4 = rectangle[1,2]
+    frame = cv2.rectangle(frame, (x1,y1),(x2,y2),(255,0,0),2)
+    frame = cv2.rectangle(frame, (x3,y3),(x4,y4),(255,0,0),2)
+    cv2.fillPoly(mask, rectangle,255)
+    cv2.imshow('Mask',mask)
+
+    edges = cv2.multiply(edges,mask)
+
+
     # Detect points that form a line
     lines = cv2.HoughLinesP(edges, 1, np.pi/180, 100, minLineLength=10, maxLineGap=250)
     # Draw lines on the image

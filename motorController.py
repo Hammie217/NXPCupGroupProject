655c77b47e213bca25f867aef5f90f47df91355f
Hammie217 <hammie217@gmail.com>
2019-11-15 19:40:47 +0000

    changed duty to percentage

--- motorController.py
+++ motorController.py
@@ -13,24 +13,32 @@ class PWMMotor():
             PWMOut = pin
         self.controllerNumber = PWMOut
         self.pwm = PWM(PWMOut, 0)
+        self.pwm.frequency = 50
+
     
     def setFrequency(self,frequency):
-        self.pwm.frequency = 50
+        self.pwm.frequency = frequency
     
     def setDuty(self,Duty):
-        self.pwm.duty_cycle = Duty
+        if((Duty>0)and(Duty<100)):
+            Duty=(Duty*0.0005)+0.05
+            self.pwm.duty_cycle = Duty
+        elif(Duty==-1):
+            self.pwm.duty_cycle = 0
+        else:
+            print("Incorrect duty (0-100)")
+            self.pwm.duty_cycle=0.05
 
     def start(self):
         self.pwm.enable()
 
     def stop(self):
+        self.setDuty(-1)
         self.pwm.close()
 
 
 motor1 = PWMMotor(32)
-motor1.setFrequency(50)
-motor1.setDuty(0.10)
-
+motor1.setDuty(100)
 motor1.start()
 motor1.stop()
 

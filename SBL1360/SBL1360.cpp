70f99585c3b6d46483c3ebe67a4ac68456f282c0
andrew <andrew.huff777@yahoo.co.uk>
2020-03-12 15:46:24 +0000

    added bluetooth to SBL lib

--- SBL1360/SBL1360.cpp
+++ SBL1360/SBL1360.cpp
@@ -3,6 +3,10 @@
 
 Serial rs232(D14, D15);
 
+#ifdef BLUETOOTH_BUF_LENGTH
+Serial bt(PTE22, PTE23); // UART2
+#endif
+
 Ticker sbl_wdog_serve;
 volatile bool sbl_wdog_flag;
 void sbl_wdog_serve_isr(void){
@@ -19,6 +23,9 @@ SBL1360::SBL1360(){
             // setup timer to service sbl wdog
             sbl_wdog_flag = 0;
             sbl_wdog_serve.attach(&sbl_wdog_serve_isr, 0.4);
+            #ifdef BLUETOOTH_BUF_LENGTH
+            bt.baud(115200);
+            #endif
         }
         
 
@@ -33,4 +40,29 @@ void SBL1360::update(void){
                 sbl_wdog_flag = 0;
                 rs232.printf("!g %d\r", speed_sp);
             }
+            #ifdef BLUETOOTH_BUF_LENGTH
+            if(bt.readable()){
+                char charFromBt;
+                charFromBt = bt.getc();
+                //pc.printf("char incoming: %c\n", charFromBt);
+                if(charFromBt == '\r'){
+                    int intFromBt = 0;
+                    for(int n=0; n<bufIndex; n++){
+                        int powerOf10 = 1;
+                        for(int m=0; m<bufIndex-n-1; m++){
+                            powerOf10 *= 10;
+                        }
+                        intFromBt += bufFromBt[n]*powerOf10; // convert array to number
+                    }
+                    bufIndex = 0;
+                    this->speed(intFromBt);
+                } else if (charFromBt > 47 && charFromBt < 58){
+                    if (bufIndex == BLUETOOTH_BUF_LENGTH) bufIndex = 0;
+                    bufFromBt[bufIndex++] = charFromBt-48;  // convert to ASCII
+                    //pc.printf("valid char not end: %d\nnext buf index: %d\n", charFromBt-48, bufIndex);
+                } else {
+                    bufIndex = 0;
+                }
+            }
+            #endif
         }
--- global_parts 2.h
+++
@@ -1,9 +0,0 @@
-#ifndef GLOBAL_OBJECTS
-#define GLOBAL_OBJECTS
-
-#include "mbed.h"
-extern Serial pc;
-
-void delayMicroseconds(unsigned int delay);
-
-#endif
